9fb2094f41861cb2db73c3af2e864447cc852832
Yuva Sai Ram <saikola014@gmail.com>
2025-05-30 08:07:29 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,18 @@
 const mysql = require('mysql2');
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',       // leave blank if you haven't set a password
+  password: '', // if using XAMPP default
   database: 'registration_db'
 });
+
+connection.connect(err => {
+  if (err) {
+    console.error('MySQL connection failed:', err);
+    return;
+  }
+  console.log('✅ MySQL connected');
+});
+
+module.exports = connection;
